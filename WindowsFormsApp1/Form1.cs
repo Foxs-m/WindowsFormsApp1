bd26ab9cb9290f5179eaa756140816deadeafd74
Home <Home@OMEN>
2021-11-18 21:52:41 +0900

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if ((comboBox1.Text == "admin") && (textBox1.Text == "1234"))
+			{
+				Form2 frm2 = new Form2();
+				frm2.Show();
+			}
+
+			else 
+			{
+				MessageBox.Show("пароль не верен,попробуйте еще раз");
+			}
+
+			if ((comboBox1.Text == "user") && (textBox1.Text == "14789"))
+			{
+				Form3 frm3 = new Form3();
+				frm3.Show();
+			}
+
+			else
+			{
+				MessageBox.Show("пароль не верен,попробуйте еще раз");
+			}
+		}
+
+		private void label3_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
+  
